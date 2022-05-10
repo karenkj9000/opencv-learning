edc125768f499e342512740d38a871cbf57a1b6d
Karen Joseph <44795656+karenkj9000@users.noreply.github.com>
2022-05-10 15:39:17 +0530

    Add files via upload

---
+++ CharacterExtraction.py
@@ -0,0 +1,43 @@
+
+import win32gui
+import win32ui
+import win32con
+from ctypes import windll
+from PIL import Image
+
+hwnd = win32gui.FindWindow(None, 'Canvas')
+
+
+left, top, right, bot = win32gui.GetClientRect(hwnd)
+
+w = right - left
+h = bot - top
+
+hwndDC = win32gui.GetWindowDC(hwnd)
+mfcDC  = win32ui.CreateDCFromHandle(hwndDC)
+saveDC = mfcDC.CreateCompatibleDC()
+
+saveBitMap = win32ui.CreateBitmap()
+saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
+
+saveDC.SelectObject(saveBitMap)
+
+result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 3)
+
+print(result)
+
+bmpinfo = saveBitMap.GetInfo()
+bmpstr = saveBitMap.GetBitmapBits(True)
+
+im = Image.frombuffer(
+    'RGB',
+    (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
+    bmpstr, 'raw', 'BGRX', 0, 1)
+
+win32gui.DeleteObject(saveBitMap.GetHandle())
+saveDC.DeleteDC()
+mfcDC.DeleteDC()
+win32gui.ReleaseDC(hwnd, hwndDC)
+
+if result == 1:
+    im.save("test.png")
